72f8fece8ee73a2ea5a40ec3a1ac1b3c652e37bf
Vikesh Shrivastav <vikesh.shrivastav.7@gmail.com>
2026-03-13 00:14:34 +0530

    feat: remove unnecessary workflow

--- deploy/aws/terraform/vpc.tf
+++ deploy/aws/terraform/vpc.tf
@@ -10,3 +10,10 @@ resource "aws_subnet" "public" {
   cidr_block              = var.public_subnet_cidrs[count.index]
   map_public_ip_on_launch = true
 }
+
+resource "aws_subnet" "private_app" {
+  count             = length(var.private_app_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_app_subnet_cidrs[count.index]
+  map_public_ip_on_launch = false
+}
