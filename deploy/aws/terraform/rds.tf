f2df5b32c171442410eaf0e8ab83c7d60c8ddd9c
Vikesh Shrivastav <vikesh.shrivastav.7@gmail.com>
2026-03-13 00:22:36 +0530

    feat: remove unnecessary workflow

--- deploy/aws/terraform/rds.tf
+++ deploy/aws/terraform/rds.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "main" {
   engine               = "postgres"
   engine_version       = "15.2"
   instance_class       = "db.t3.micro"
-  name                 = var.db_name
+  db_name              = var.db_name
   username             = var.db_user
   password             = var.db_password
   db_subnet_group_name = aws_db_subnet_group.main.name
