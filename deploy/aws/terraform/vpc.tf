9550029552179082140d0342692e50947986f9f3
Vikesh Shrivastav <vikesh.shrivastav.7@gmail.com>
2026-03-13 01:20:51 +0530

    feat: remove unnecessary workflow

--- deploy/aws/terraform/vpc.tf
+++ deploy/aws/terraform/vpc.tf
@@ -15,5 +15,6 @@ resource "aws_subnet" "private_app" {
   count             = length(var.private_app_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_app_subnet_cidrs[count.index]
+  availability_zone = ["ap-south-1a", "ap-south-1c"][count.index]
   map_public_ip_on_launch = false
 }
