27fd28bc7f21757ce5decf7b8dc082dd768965ea
Vikesh Shrivastav <vikesh.shrivastav.7@gmail.com>
2026-03-13 17:40:22 +0530

    feat: infra update

--- deploy/aws/terraform/vpc.tf
+++ deploy/aws/terraform/vpc.tf
@@ -1,3 +1,21 @@
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
