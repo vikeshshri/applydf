7d87a1c645108b79189fcbc171eb64a62f1bce94
Vikesh Shrivastav <vikesh.shrivastav.7@gmail.com>
2026-03-14 02:30:53 +0530

    feat:infra update

--- deploy/aws/terraform/ecs.tf
+++ deploy/aws/terraform/ecs.tf
@@ -19,4 +19,11 @@ resource "aws_security_group" "ecs" {
   name        = "ecs-sg"
   description = "ECS access"
   vpc_id      = aws_vpc.main.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
 }
--- deploy/aws/terraform/route53.tf
+++ deploy/aws/terraform/route53.tf
@@ -1,9 +1,29 @@
 resource "aws_route53_zone" "main" {
   name = var.domain_name
 }
-
+}
+resource "aws_route53_record" "api" {
 resource "aws_route53_record" "api" {
   zone_id = aws_route53_zone.main.id
+  name    = "api.${var.domain_name}"
+  type    = "A"
+  alias {
+    name                   = aws_lb.api.dns_name
+    zone_id                = aws_lb.api.zone_id
+    evaluate_target_health = true
+  }
+}
+  zone_id = aws_route53_zone.main.id
+resource "aws_route53_record" "app" {
+  zone_id = aws_route53_zone.main.id
+  name    = "app.${var.domain_name}"
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.frontend.domain_name
+    zone_id                = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
   name    = "api.${var.domain_name}"
   type    = "A"
   ttl     = 300
