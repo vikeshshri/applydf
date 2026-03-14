fd3d57ffcd0ab9443814e505139b4383bdc67172
Vikesh Shrivastav <vikesh.shrivastav.7@gmail.com>
2026-03-14 19:39:58 +0530

    feat: terraform modified

--- deploy/aws/terraform/route53.tf
+++ deploy/aws/terraform/route53.tf
@@ -1,8 +1,6 @@
 resource "aws_route53_zone" "main" {
   name = var.domain_name
 }
-}
-resource "aws_route53_record" "api" {
 resource "aws_route53_record" "api" {
   zone_id = aws_route53_zone.main.id
   name    = "api.${var.domain_name}"
@@ -13,7 +11,6 @@ resource "aws_route53_record" "api" {
     evaluate_target_health = true
   }
 }
-  zone_id = aws_route53_zone.main.id
 resource "aws_route53_record" "app" {
   zone_id = aws_route53_zone.main.id
   name    = "app.${var.domain_name}"
@@ -23,9 +20,4 @@ resource "aws_route53_record" "app" {
     zone_id                = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
-}
-  name    = "api.${var.domain_name}"
-  type    = "A"
-  ttl     = 300
-  records = ["1.2.3.4"] # Replace with actual API LB IP
 }
